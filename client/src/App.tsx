4db02caab8d36e47e6acbe646458f99dda9914af
Elguja Modebadze <gujiko.modebadze@gmail.com>
2026-07-28 18:37:16 +0400

    initialise frontend with vite, typscript, react, tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <div>
+        <h1 className='text-2xl font-bold'>List of Destinations</h1>
+      </div>
+    </div>
+  );
+}
